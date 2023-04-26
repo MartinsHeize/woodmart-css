a167e2b9f5e1d652353cd84be37bbb9ce271f731
MartinsHeize <95350610+MartinsHeize@users.noreply.github.com>
2023-04-26 19:22:40 +0300

    Initial commit

---
+++ files/404/404.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The template for displaying 404 pages (Not Found)
+ */
+
+get_header(); ?>
+
+<div class="site-content col-12" role="main">
+
+		<?php
+			echo do_shortcode( '[html_block id="8829"]' );
+
+		?>
+	</div><!-- .page-content -->
+
+</div><!-- .site-content -->
+
+<?php get_footer(); ?>
